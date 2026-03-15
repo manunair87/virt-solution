57678cc5f8f1673ae87bf369008bcb806a8a4b9f
Manoj <manojuknair87@gmail.com>
2026-03-15 21:23:01 +0530

    My first web page setup - just echo only

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Virtual Solutions Pvt Ltd</title>
+</head>
+<body>
+    <h1>Welcome to Virtual Solutions Pvt Ltd.</h1>
+    <?php
+        echo "<p>Our PHP server is working correctly!</p>";
+    ?>
+</body>
+</html>
