a4a224dd9b43e6f8cfc2135032717de7953c8a4e
Manoj <manojuknair87@gmail.com>
2026-03-16 11:20:47 +0530

    Added db connection details, dummy page & gitignore

---
+++ db_connect.example.php
@@ -0,0 +1,7 @@
+<?php
+$servername = "localhost";
+$username = "your_db_user"; 
+$password = "your_db_password"; 
+$dbname = "virtual_solutions_db";
+$conn = new mysqli($servername, $username, $password, $dbname);
+?>
